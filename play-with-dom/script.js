ceba9623c051b28c95b4a5b9211b468901e3c424
towhidappaloy <towhid@appaloy.com>
2022-05-12 17:07:33 +0600

    Play With Dom

---
+++ play-with-dom/script.js
@@ -0,0 +1,16 @@
+// console.log(document);
+// console.dir(document);
+// console.log(document.title);
+// console.log(document.URL);
+// console.log(document.domain);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.title = 'Play With Him');
+// console.log(document.all);
+
+// for(element of document.all){
+//     console.log(element);
+// }
+
+
+console.log(document.all[5]);
